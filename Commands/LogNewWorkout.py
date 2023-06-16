e292ec0dfa17be14af0e2efd8b68ed845c59a032
stijuh <stije.it@gmail.com>
2023-06-17 00:39:35 +0200

    Added working logging of NEW workouts

--- Commands/LogNewWorkout.py
+++ Commands/LogNewWorkout.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from BaseCommand import BaseCommand
 from Common.Constants import BASE_URL
 from Common.Methods import checkStatusCode, getDataFromResponse
@@ -14,20 +16,19 @@ class LogNewWorkoutCommand(BaseCommand):
 
         workouts = getDataFromResponse(workoutsResponse)
 
-        logWorkoutView = WorkoutDropDownView(workouts)
+        logWorkoutView = WorkoutDropDownView(workouts, isNew=True, workoutSelectCallback=self.workoutSelectCallback)
         await self.message.channel.send(view=logWorkoutView)
 
-        # response = self.session.post(f"{BASE_URL}/gains/workout", json=addWorkoutPayload)
-        #
-        # if self.responsePositive(response):
-        #     await self.sendMessage("Successfully added workout. Let's put the _fit_ around f**ict**!")
-        #     await asyncio.sleep(4)
-        #     await self.sendMessage("Still doesn't really roll of the tongue that well..")
-        # else:
-        #     if response.status_code == 409:
-        #         await self.sendMessage("This workout is already added to your account!")
-        #         raise Exception
-        #     else:
-        #         await checkStatusCode(response, self.message.channel, self.workoutType)
+    async def workoutSelectCallback(self, selected):
+        response = self.session.post(f"{BASE_URL}/gains/workout", json={"workoutType": selected})
 
-        # return response
+        if self.responsePositive(response):
+            await self.sendMessage("Successfully added workout. Let's put the _fit_ around f**ict**!")
+            await asyncio.sleep(4)
+            await self.sendMessage("Still doesn't really roll of the tongue that well..")
+        else:
+            if response.status_code == 409:
+                await self.sendMessage("This workout is already added to your account!")
+                raise Exception
+            else:
+                await checkStatusCode(response, self.message.channel)
--- Views/WorkoutDropDownView.py
+++ Views/WorkoutDropDownView.py
@@ -5,19 +5,21 @@ from discord.ui import View
 
 
 class WorkoutDropDownView(View):
-    def __init__(self, workoutOptions, workoutSelectCallback=None):
+    def __init__(self, workoutOptions, isNew=False, workoutSelectCallback=None):
         super().__init__()
         self.workoutOptions = workoutOptions
         self.workoutSelectCallback = workoutSelectCallback
-        self.add_item(self.createWorkoutSelect())
+        self.add_item(self.createWorkoutSelect(isNew))
 
-    def createWorkoutSelect(self):
+    def createWorkoutSelect(self, isNew):
         selectOptions = []
 
         optionCount = 0
         for workout in self.workoutOptions:
+            typeValue = workout["type"] if isNew else workout["category"]
+
             selectOptions.append(SelectOption(label=workout["type"],
-                                              value=workout["category"] + "-" + str(optionCount),
+                                              value=str(typeValue) + "-" + str(optionCount),
                                               emoji=emojiPerCategory[workout["category"]],
                                               default=False))
             optionCount += 1
@@ -25,11 +27,13 @@ class WorkoutDropDownView(View):
         select = Select(placeholder="Select a workout", options=selectOptions)
 
         async def workoutSelectCallback(interaction: discord.Interaction):
-            if self.workoutSelectCallback is None:
-                await interaction.message.channel.send(f"epic i like the category {select.values[0]} too :sunglasses:")
-            else:
-                await self.workoutSelectCallback(select.values[0])
+            value = select.values[0].split("-")[0]
             await interaction.response.defer()
+
+            if self.workoutSelectCallback is None:
+                await interaction.message.channel.send(f"epic i like the {value} too :sunglasses:")
+            else:
+                await self.workoutSelectCallback(selected=value)
 
         select.callback = workoutSelectCallback
         return select
