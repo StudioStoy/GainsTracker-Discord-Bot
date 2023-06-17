61c1c3b9b122c32e057d7778d4420876905c1c53
Junmii <joys.it.projects@gmail.com>
2023-06-17 22:45:57 +0200

    Tried to show modal when adding new workout

--- Commands/LogNewWorkout.py
+++ Commands/LogNewWorkout.py
@@ -1,12 +1,18 @@
 import asyncio
 
+import discord
+
 from BaseCommand import BaseCommand
 from Common.Constants import BASE_URL
 from Common.Methods import checkStatusCode, getDataFromResponse
+from Views.LogWorkoutModal import LogWorkoutModal
 from Views.WorkoutDropDownView import WorkoutDropDownView
 
 
 class LogNewWorkoutCommand(BaseCommand):
+    def __init__(self):
+        self.workouts = []
+
     async def execute(self):
         workoutsResponse = self.session.get(f"{BASE_URL}/catalog/workout")
 
@@ -14,23 +20,31 @@ class LogNewWorkoutCommand(BaseCommand):
             await checkStatusCode(workoutsResponse, self.message.channel)
             return
 
-        workouts = getDataFromResponse(workoutsResponse)
+        self.workouts = getDataFromResponse(workoutsResponse)
 
-        logWorkoutView = WorkoutDropDownView(workouts, mutateWorkoutCallback=self.createNewWorkoutCallback)
+        logWorkoutView = WorkoutDropDownView(self.workouts, mutateWorkoutCallback=self.createNewWorkoutCallback)
         await self.message.channel.send(view=logWorkoutView)
 
     async def createNewWorkoutCallback(self, selectedDict, interaction=None):
         response = self.session.post(f"{BASE_URL}/gains/workout", json={"workoutType": selectedDict["type"]})
+        selectedDict["id"] = getDataFromResponse(response)
 
-        await interaction.response.defer()
         if self.responsePositive(response):
             epicMessage = await self.sendMessage("Successfully added workout. Let's put the _fit_ around f**ict**!")
+            workoutModal = LogWorkoutModal(selectedDict, session=self.session)
+            await interaction.response.send_modal(workoutModal)
             await asyncio.sleep(4)
             await epicMessage.edit(content="Successfully added workout. Let's put the _fit_ around f**ict**!\n\n"
                                            "Still doesn't really roll of the tongue that well..")
+
         else:
+            await interaction.response.defer()
             if response.status_code == 409:
                 await self.sendMessage("This workout is already added to your account!")
                 raise Exception
             else:
                 await checkStatusCode(response, self.message.channel)
+
+    # async def workoutSelectCallback(self, selectedDict, interaction: discord.Interaction = None):
+    #     workoutModal = LogWorkoutModal(selectedDict)
+    #     await interaction.response.send_modal(workoutModal)
