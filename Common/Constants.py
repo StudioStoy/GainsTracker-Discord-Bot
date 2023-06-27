aa5e95b9a39d5649a42f603c13bebf08f80ece0a
Stijuh <stijevoh@gmail.com>
2023-06-27 23:10:59 +0200

    Edited constant, should become an environment variable soon.

--- Common/Constants.py
+++ Common/Constants.py
@@ -1,5 +1,6 @@
 GAINS_VERSION = "1.8"
 GAINS_BOT = "<@1117078463187271680>"
-# BASE_URL = "http://aperture:420"
-BASE_URL = "http://localhost:5045"
+BASE_URL = "http://aperture:420"
+# BASE_URL = "http://localhost:5045"
 # BASE_URL = "http://host.docker.internal:420"
+# TODO: make BASE_URL an environment variable.
